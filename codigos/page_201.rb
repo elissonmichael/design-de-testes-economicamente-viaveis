1a05641eb7b154e898fc9bbd7c5168c82a22f618
Élisson Michael <elissonmichael@yahoo.com.br>
2018-09-04 12:08:26 -0300

    Ref Issue #7 | Adiciona Tradução da Página 201

---
+++ codigos/page_201.rb
@@ -0,0 +1,31 @@
+class Roda
+  attr_reader :aro, :pneu
+  def initialize(aro, pneu)
+    @aro  = aro
+    @pneu = pneu
+  end
+
+  def diametro
+    aro + (pneu * 2)
+  end
+#...
+end
+
+class Engrenagem
+  attr_reader :roda_dentada, :marcha, :aro, :roda
+  def initialize(argumentos)
+    @roda_dentada = argumentos[:roda_dentada]
+    @marcha       = argumentos[:marcha]
+    @aro          = argumentos[:aro]
+    @roda         = argumentos[:roda]
+  end
+
+  def polegadas
+    relacao * Roda.new(aro, pneu).diametro
+  end
+
+  def relacao
+    roda_dentada / marcha.to_f
+  end
+#...
+end
